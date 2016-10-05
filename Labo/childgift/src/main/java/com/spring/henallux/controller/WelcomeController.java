115804be1e61d5d7767052086d0baf100667b0ce
DamienJacques900 <damien.jacques900@gmail.com>
2016-10-05 15:18:00 +0200

    Labo 3

--- Labo/childgift/src/main/java/com/spring/henallux/controller/WelcomeController.java
+++ Labo/childgift/src/main/java/com/spring/henallux/controller/WelcomeController.java
@@ -2,8 +2,11 @@ package com.spring.henallux.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.henallux.model.MagicKeyForm;
 
 @Controller
 @RequestMapping(value="/welcome")
@@ -12,6 +15,13 @@ public class WelcomeController
 	@RequestMapping(method=RequestMethod.GET)
 	public String home(Model model)
 	{
+		model.addAttribute("magicKeyForm", new MagicKeyForm());
 		return "integrated:welcome";
+	}
+	
+	@RequestMapping(value="/send", method=RequestMethod.POST)
+	public String getFormData(Model model, @ModelAttribute(value="magicKeyForm") MagicKeyForm form)
+	{
+		return ""; //avant de trouver le bon truc A MODIFIER !!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
---
+++ Labo/childgift/src/main/java/com/spring/henallux/model/MagicKeyForm.java
@@ -0,0 +1,21 @@
+package com.spring.henallux.model;
+
+public class MagicKeyForm 
+{
+	private String magicKey;
+	
+	public MagicKeyForm()
+	{
+		
+	}
+	
+	public void setMagicKey(String mk)
+	{
+		magicKey = mk;
+	}
+	
+	public String getMagicKey()
+	{
+		return magicKey;
+	}
+}
